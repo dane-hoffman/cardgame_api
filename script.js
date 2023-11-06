273bb2b83c3d177be3fc80bff2f49bce20d45ba6
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-06 13:46:15 -0700

    Grabbed #playerOutput element and set = to const playerCardOutput.

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ const button = document.querySelector('button');
     //Create an event listener for the button
 button.addEventListener('click', () => {
     console.log("Button Clicked!")
-
-})
     //Make API call for "Player" card
-    
+        //Grab the "Player" card output
+    const playerCardOutput = document.querySelector('#playerOutput');
+    console.log (playerCardOutput);
     //Display "Player" card
     //Compare "House" card vs. "player" card
-        //Highest card value wins
-        //Display "[House/Player] wins!"
+    //Highest card value wins
+    //Display "[House/Player] wins!"
+})
+
+    

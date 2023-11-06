f8c5a646fc010cae4cb76360ed8f0101aea26de8
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-06 13:21:58 -0700

    Planned out high-level psuedocode.

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-
-console.log (button);
+//Page loads
+    //Make API call for "House" card
+    //Diplay "House" card
+//User clicks "Draw Card" button,
+    //Make API cal for "Player" card
+    //Display "Player" card
+    //Compare "House" card vs. "player" card
+        //Highest card value wins
+        //Display "[House/Player] wins!"

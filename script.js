ab28b9623777b454ebbf36633cdce6d152cd1d64
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-06 13:27:33 -0700

    Grabbed button to initialize API call; need to create event listener on button.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //Page loads
     //Make API call for "House" card
     //Diplay "House" card
-//User clicks "Draw Card" button,
+//User clicks "Draw Card" button
+const button = document.querySelector('button');
+console.log (button);
     //Make API cal for "Player" card
     //Display "Player" card
     //Compare "House" card vs. "player" card

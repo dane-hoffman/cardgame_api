8361d3bac77792cda1483b0a95c7c151c2f9c4ff
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-07 12:42:50 -0700

    Psuedocoded and restructed variables in order to better organize next step of repeating the previous steps of the playerCardObejct for the houseCardObject.

--- script.js
+++ script.js
@@ -4,20 +4,32 @@
 //User clicks "Draw Card" button
 const button = document.querySelector('button');//Grab the "Draw Card" button
 const playerOutput = document.querySelector('#playerOutput');//Grab the "Player" card output
+//Grab the houseOutput
     
 button.addEventListener('click', async () => {//Create an event listener for the button
-    //Make API call for "Player" card
-    const playerCardDraw = await fetch(`https://www.deckofcardsapi.com/api/deck/new/draw/?count=1`);
-    const playerCardData = await playerCardDraw.json();
-    const playerCard = playerCardData.cards;
+    //Make API call for Two cards
+    const cardDraw = await fetch(`https://www.deckofcardsapi.com/api/deck/new/draw/?count=2`);
+    const cardData = await cardDraw.json();
+    const cards = cardData.cards;
     //Display "Player" card
-    const newCardObject = document.createElement('p');
-    newCardObject.textContent = playerCard[0].value + ` of ` + playerCard[0].suit;
+    const playerCardObject = document.createElement('p');
+    playerCardObject.textContent = cards[0].value + ` of ` + cards[0].suit;
+    playerOutput.append(playerCardObject);
+    console.log (cards);
 
-    playerOutput.append(newCardObject);
-    console.log (playerCard);
+    //Repeat steps for 'house' card
+    //Make API call for 'House" card
+    //Additional call not required; single call provides two cards
+    //retrieve [1]
+    // const houseCardDraw = document.createElement('p');
+    // const
+
+
+    //Display "House" card
+
+
 
     //Compare "House" card vs. "player" card
-    //Highest card value wins
-    //Display "[House/Player] wins!"
-})
+        //Highest card value wins
+        //Display "[House/Player] wins!"
+});

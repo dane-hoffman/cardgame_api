901802c66590eb137788354a8d17ccbd45cab1b4
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-07 12:52:32 -0700

    Grabbed #houseOutput and added .textContent and .append to houseOutput

--- script.js
+++ script.js
@@ -5,25 +5,29 @@
 const button = document.querySelector('button');//Grab the "Draw Card" button
 const playerOutput = document.querySelector('#playerOutput');//Grab the "Player" card output
 //Grab the houseOutput
+const houseOutput = document.querySelector('#houseOutput');
     
 button.addEventListener('click', async () => {//Create an event listener for the button
     //Make API call for Two cards
     const cardDraw = await fetch(`https://www.deckofcardsapi.com/api/deck/new/draw/?count=2`);
     const cardData = await cardDraw.json();
     const cards = cardData.cards;
+    console.log (cards);
+    
     //Display "Player" card
     const playerCardObject = document.createElement('p');
     playerCardObject.textContent = cards[0].value + ` of ` + cards[0].suit;
     playerOutput.append(playerCardObject);
-    console.log (cards);
+    console.log(playerCardObject);
 
     //Repeat steps for 'house' card
     //Make API call for 'House" card
     //Additional call not required; single call provides two cards
     //retrieve [1]
-    // const houseCardDraw = document.createElement('p');
-    // const
-
+    const displayHouseCard = document.createElement('p');
+    displayHouseCard.textContent = "Hello";
+    console.log(houseOutput);
+    houseOutput.append(displayHouseCard);
 
     //Display "House" card
 

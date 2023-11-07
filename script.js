e67cecc40a2420f07f01818880873db0fca77c9c
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-07 14:37:13 -0700

    Successfully displays the playerCard img '

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 //Page loads
-//User clicks "Draw Card" button
 const button = document.querySelector('button');//Grab the "Draw Card" button
 const playerOutput = document.querySelector('#playerOutput');//Grab the "Player" card output
 const houseOutput = document.querySelector('#houseOutput');//Grab the houseOutput
-//Grab outcomeOutput
-const outcomeOutput = document.querySelector('#outcomeOutput');
+const outcomeOutput = document.querySelector('#outcomeOutput');//Grab outcomeOutput
+//Grab the playerCard section
+const playerCardSection = document.querySelector('#playerCard');
+//Grab the playerCardIMG
+const playerCardIMG = document.querySelector('#playerCardIMG');
 
-    
+    //User clicks "Draw Card" button
 button.addEventListener('click', async () => {//Create an event listener for the button
     //Make API call for Two cards
     const cardDraw = await fetch(`https://www.deckofcardsapi.com/api/deck/new/draw/?count=2`);
     const cardData = await cardDraw.json();
     const cards = cardData.cards;
+
     console.log (cards);
     
-    //Display "Player" card
+    //Display "Player" card Value and Suit
     const playerCardObject = document.createElement('p');
     playerCardObject.textContent = cards[0].value + ` of ` + cards[0].suit;
     playerOutput.innerHTML = '';
     playerOutput.append(playerCardObject);
+
+    //Display Player card image
+    const playerCardImage = document.createElement('img');
+    playerCardImage.src = cards[0].image;
+    playerCardSection.appendChild(playerCardImage);    
 
     //Display "House" card
     const displayHouseCard = document.createElement('p');//Additional call not required; single call provides two cards
@@ -28,14 +36,14 @@ button.addEventListener('click', async () => {//Create an event listener for the
 
     //Compare "House" card vs. "player" card
         //Highest card value wins
-        //Display "[House/Player] wins!"
     if (cards[0].value > cards[1].value) {//if player card is greater than house card
-        console.log("Player card wins!");//console log/display "Player Card Wins!"
         outcomeMessage = "Player Card Wins!";
     } else if(cards[1].value > cards[0].value) {//if house card is greater than player card
-        console.log("The  House ALWAYS Wins!");
         outcomeMessage = "The House ALWAYS Wins!";
+    } else if (cards[0].value = cards[1].value) {
+        outcomeMessage = "It's a DRAW!"
     }
+    //Need to deal with Face Cards
     
     //Display Outcome Message
     const displayOutcome = document.createElement('h4');

d6329e80d3f82a30cc1ad65135c710bdb8034088
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-07 13:05:02 -0700

    Successfully compared cards[0].value > cards[1].value; console log win.

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 //Page loads
-    //Make API call for "House" card
-    //Diplay "House" card
 //User clicks "Draw Card" button
 const button = document.querySelector('button');//Grab the "Draw Card" button
 const playerOutput = document.querySelector('#playerOutput');//Grab the "Player" card output
-//Grab the houseOutput
-const houseOutput = document.querySelector('#houseOutput');
+const houseOutput = document.querySelector('#houseOutput');//Grab the houseOutput
     
 button.addEventListener('click', async () => {//Create an event listener for the button
     //Make API call for Two cards
@@ -17,23 +14,19 @@ button.addEventListener('click', async () => {//Create an event listener for the
     //Display "Player" card
     const playerCardObject = document.createElement('p');
     playerCardObject.textContent = cards[0].value + ` of ` + cards[0].suit;
+    playerOutput.innerHTML = '';
     playerOutput.append(playerCardObject);
 
     //Display "House" card
-    //Repeat steps for 'house' card
-    //Make API call for 'House" card
-    //Additional call not required; single call provides two cards
-    //retrieve [1]
-    const displayHouseCard = document.createElement('p');
-    displayHouseCard.textContent = cards[1].value + ` of ` + cards[1].suit;
+    const displayHouseCard = document.createElement('p');//Additional call not required; single call provides two cards
+    displayHouseCard.textContent = cards[1].value + ` of ` + cards[1].suit; //retrieve [1]
     houseOutput.innerHTML = '';
     houseOutput.append(displayHouseCard);
 
-
-
-
-
     //Compare "House" card vs. "player" card
+    if (cards[0].value > cards[1].value) {
+        console.log("Player card wins!");
+    }
         //Highest card value wins
         //Display "[House/Player] wins!"
 });

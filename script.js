654d4e1f30bd3b4e62dfdfe03c73224d0878ad07
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-07 20:59:47 -0700

    Display playerCardContent and houseCardContent, fresh info each time.

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
-//Page loads
-const button = document.querySelector('button');//Grab the "Draw Card" button
-const playerOutput = document.querySelector('#playerOutput');//Grab the "Player" card output
-const houseOutput = document.querySelector('#houseOutput');//Grab the houseOutput
-const outcomeOutput = document.querySelector('#outcomeOutput');//Grab outcomeOutput
-//Grab the playerCard section
-const playerCardSection = document.querySelector('#playerCard');
-//Grab the playerCardIMG
+// Page loads
+const button = document.querySelector('button');
+const playerOutput = document.querySelector('#playerOutput');
+const houseOutput = document.querySelector('#houseOutput');
+const outcomeOutput = document.querySelector('#outcomeOutput');
 const playerCardIMG = document.querySelector('#playerCardIMG');
+const playerCardContent = document.querySelector('#playerCardContent');
+const houseCardContent = document.querySelector('houseCardContent');
 
-    //User clicks "Draw Card" button
-button.addEventListener('click', async () => {//Create an event listener for the button
-    //Make API call for Two cards
-    const cardDraw = await fetch(`https://www.deckofcardsapi.com/api/deck/new/draw/?count=2`);
+// User clicks "Draw Card" button
+button.addEventListener('click', async () => {
+    // Reset previous card and outcome information
+    playerCardContent.innerHTML = '';
+    houseOutput.innerHTML = '';
+    outcomeOutput.innerHTML = '';
+
+    // Make API call for two cards
+    const cardDraw = await fetch('https://www.deckofcardsapi.com/api/deck/new/draw/?count=2');
     const cardData = await cardDraw.json();
     const cards = cardData.cards;
 
-    console.log (cards);
-    
-    //Display "Player" card Value and Suit
+    // Define and Display playerCard; Values, Suit, and src Image
     const playerCardObject = document.createElement('p');
     playerCardObject.textContent = cards[0].value + ` of ` + cards[0].suit;
-    playerOutput.innerHTML = '';
-    playerOutput.append(playerCardObject);
-
-    //Display Player card image
     const playerCardImage = document.createElement('img');
     playerCardImage.src = cards[0].image;
-    playerCardSection.appendChild(playerCardImage);    
+    playerCardContent.append(playerCardObject, playerCardImage);
 
-    //Display "House" card
-    const displayHouseCard = document.createElement('p');//Additional call not required; single call provides two cards
-    displayHouseCard.textContent = cards[1].value + ` of ` + cards[1].suit; //retrieve [1]
-    houseOutput.innerHTML = '';
-    houseOutput.append(displayHouseCard);
+    // Define and Display "House" card
+    const displayHouseCard = document.createElement('p');
+    displayHouseCard.textContent = cards[1].value + ` of ` + cards[1].suit;
+    const houseCardImage = document.createElement('img');
+    houseCardImage.src = cards[1].image;
+    houseOutput.append(displayHouseCard, houseCardImage);
 
-    //Compare "House" card vs. "player" card
-        //Highest card value wins
-    if (cards[0].value > cards[1].value) {//if player card is greater than house card
+    // Compare "House" card vs. "Player" card
+    let outcomeMessage = '';
+    if (cards[0].value > cards[1].value) {
         outcomeMessage = "Player Card Wins!";
-    } else if(cards[1].value > cards[0].value) {//if house card is greater than player card
+    } else if (cards[1].value > cards[0].value) {
         outcomeMessage = "The House ALWAYS Wins!";
-    } else if (cards[0].value = cards[1].value) {
-        outcomeMessage = "It's a DRAW!"
+    } else if (cards[0].value === cards[1].value) {
+        outcomeMessage = "It's a DRAW!";
     }
-    //Need to deal with Face Cards
-    
-    //Display Outcome Message
+
+    // Display Outcome Message
     const displayOutcome = document.createElement('h4');
     displayOutcome.textContent = outcomeMessage;
-    outcomeOutput.innerHTML = '';
     outcomeOutput.append(displayOutcome);
-
 });
